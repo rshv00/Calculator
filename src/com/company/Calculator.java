6549020d14f81ed94862c29df97706fb0d06a5b5
Roman <shevromanvk@gmail.com>
2016-06-01 23:09:00 +0300

    -

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -6,6 +6,7 @@ package com.company;
 public class Calculator {
     public String calculate(String expression) {
         char c0 = expression.charAt(0);
+        char c4 = expression.charAt(4);
         if (expression.length() == 1 && Character.isDigit(c0)) {
             return expression;
         }
@@ -62,13 +63,41 @@ public class Calculator {
             String res = String.valueOf(i0 + i3);
             return res;
         }
-        char c4 = expression.charAt(3);
-        if (expression.length() == 5
-                && Character.isDigit(c0)
-                && Character.isDigit(c1)
-                && expression.charAt(2) == '+'
-                && Character.isDigit(c3)
-                && Character.isDigit(c4)) {
+
+        if (expression.length() == 4 && Character.isDigit(c0) && Character.isDigit(c1) && expression.charAt(2) == '-' && Character.isDigit(c3)) {
+            String s0 = String.valueOf(c0);
+            String s1 = String.valueOf(c1);
+            String add = s0 + s1;
+            String s3 = String.valueOf(c3);
+            Integer i0 = Integer.valueOf(add);
+            Integer i3 = Integer.valueOf(s3);
+            String res = String.valueOf(i0 - i3);
+            return res;
+        }
+        if (expression.length() == 4 && Character.isDigit(c0) && Character.isDigit(c1) && expression.charAt(2) == '+' && Character.isDigit(c3)) {
+            String s0 = String.valueOf(c0);
+            String s1 = String.valueOf(c1);
+            String add = s0 + s1;
+            String s3 = String.valueOf(c3);
+            Integer i0 = Integer.valueOf(add);
+            Integer i3 = Integer.valueOf(s3);
+            String res = String.valueOf(i0 + i3);
+            return res;
+        }
+
+        if (expression.length() == 5 && Character.isDigit(c0) && Character.isDigit(c1) && expression.charAt(2) == '-' && Character.isDigit(c3) && Character.isDigit(c4)) {
+            String s0 = String.valueOf(c0);
+            String s1 = String.valueOf(c1);
+            String add = s0 + s1;
+            String s3 = String.valueOf(c3);
+            String s4 = String.valueOf(c4);
+            String add2 = s3 + s4;
+            Integer i0 = Integer.valueOf(add);
+            Integer i1 = Integer.valueOf(add2);
+            String res = String.valueOf(i0 - i1);
+            return res;
+        }
+        if (expression.length() == 5 && Character.isDigit(c0) && Character.isDigit(c1) && expression.charAt(2) == '+' && Character.isDigit(c3) && Character.isDigit(c4)) {
             String s0 = String.valueOf(c0);
             String s1 = String.valueOf(c1);
             String add = s0 + s1;
@@ -78,38 +107,6 @@ public class Calculator {
             Integer i0 = Integer.valueOf(add);
             Integer i1 = Integer.valueOf(add2);
             String res = String.valueOf(i0 + i1);
-            return res;
-        }
-        if (expression.length() == 3
-                && Character.isDigit(c0)
-                && Character.isDigit(c1)
-                && expression.charAt(2) == '-'
-                && Character.isDigit(c3)
-                && Character.isDigit(c4)) {
-            String s0 = String.valueOf(c0);
-            String s1 = String.valueOf(c1);
-            String add = s0 + s1;
-            String s3 = String.valueOf(c3);
-            String s4 = String.valueOf(c4);
-            String add2 = s3 + s4;
-            Integer i0 = Integer.valueOf(add);
-            Integer i1 = Integer.valueOf(add2);
-            String res = String.valueOf(i0 - i1);
-            return res;
-        }
-        if (expression.length() == 4
-                && Character.isDigit(c0)
-                && expression.charAt(1) == '-'
-                && Character.isDigit(c2)
-                && Character.isDigit(c3)
-                ) {
-            String s0 = String.valueOf(c0);
-            String s2 = String.valueOf(c3);
-            String s3 = String.valueOf(c4);
-            String add = s3 + s2;
-            Integer i0 = Integer.valueOf(s0);
-            Integer i1 = Integer.valueOf(add);
-            String res = String.valueOf(i0 - i1);
             return res;
         }
 
