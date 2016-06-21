cef159a8401a5e593afa9b29f97111d8fd1280e2
roman <shevromanvk@gmail.com>
2016-06-21 18:35:49 +0300

    +

--- src/com/company/ExpressionParser.java
+++ src/com/company/ExpressionParser.java
@@ -1,19 +1,25 @@
 package com.company;
 
-import com.company.tokens.IntValue;
 import com.company.tokens.Token;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * Created by User on 30.05.2016.
  */
 public class ExpressionParser {
     ArrayList<Token> parseExpression(String exp) {
-        int v = Integer.valueOf(exp);
+        String tokens2 = String.valueOf(exp);
 
-        ArrayList<Token> tokens = new ArrayList<>();
-        tokens.add(new IntValue(v));
-        return tokens;
+        StringTokenizer st = new StringTokenizer(tokens2);
+            while(st.hasMoreTokens())
+            {
+                Integer i0= Integer.parseInt(st.nextToken());
+
+            }
+
+
+            return null;
+        }
     }
-}
